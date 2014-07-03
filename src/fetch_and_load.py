847f59ed76c1eab078b46690f46abe13b5c5cfc2
Jason S. Ninneman <jsn@mbar.us>
2014-07-02 22:41:20 -0800

    Working testbed for fetching and loading the station data.

--- src/fetch_and_load.py
+++ src/fetch_and_load.py
@@ -1,28 +1,59 @@
 #!/usr/bin/python3
 
-from ftplib import FTP
 import sqlite3
+import urllib.request
+import sys
+import logging
 
-lines = []
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+log_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(log_format)
+logger.addHandler(console_handler)
 
-def get_lines(block):
-    global lines
-    lines.append(block.split("\n"))
+logger.info("getting station list")
+#r = urllib.request.urlopen("ftp://ftp.ncdc.noaa.gov/pub/data/igra/igra-stations.txt")
+r = urllib.request.urlopen("file:///tmp/igrabrowser/igra-stations.txt")
+block = r.read().decode("ascii")
+lines = block.split("\n")
 
-ftp = FTP("ftp.ncdc.noaa.gov")
-ftp.login()
-ftp.cwd("/pub/data/igra")
-ftp.retrlines("RETR igra-stations.txt", get_lines)
-ftp.quit()
+station_list = []
 
 for line in lines:
-    data_string = line[0]
-    country_code = data_string[0:2]
-    print(country_code)
+    # ignore blank lines
+    if line == "":
+        continue
 
-#conn = sqlite3.connect("igrabrowser.db")
-#c = conn.cursor()
+    row = (
+        line[0:2], # country code
+        int(line[4:9]), # station number
+        line[11:46], # station name
+        float(line[47:53]), # latitude
+        float(line[54:61]), # longitude
+        int(line[62:66]), # elevation
+        line[67:68], # GUAN code
+        line[68:69], # LKS network code
+        line[69:70], # composite station code
+        int(line[72:76]), # first year of record
+        int(line[77:81]), # last year of record
+        )
+    station_list.append(row)
 
-#c.execute("""
 
-#""")
+logger.info("loading database")
+conn = sqlite3.connect("/tmp/igrabrowser/igrabrowser.db")
+c = conn.cursor()
+
+c.execute("""
+create table if not exists station_list
+  (country_code, station_number PRIMARY KEY, station_name, latitude, longitude, elevation, GUAN_code, LKS_code, composite_code, first_year, last_year)
+""")
+
+for row in station_list:
+    c.execute("""
+    replace into station_list values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, row)
+
+conn.commit()
+c.close()
